d1b1b28e825b7108c4afa902ea136e1b66ae9868
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2026-01-05 13:34:06 +0900

    Create sw.js

---
+++ web-bunny/sw.js
@@ -0,0 +1,41 @@
+const CACHE = "bunny-cache-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.webmanifest",
+  "./assets/bunny.png",
+  "./assets/icon-192.png",
+  "./assets/icon-512.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil((async () => {
+    const c = await caches.open(CACHE);
+    await c.addAll(ASSETS);
+    self.skipWaiting();
+  })());
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)));
+    self.clients.claim();
+  })());
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith((async () => {
+    const cached = await caches.match(e.request);
+    if (cached) return cached;
+    try {
+      const res = await fetch(e.request);
+      return res;
+    } catch {
+      // オフラインで未キャッシュの場合は最低限トップへ
+      return caches.match("./index.html");
+    }
+  })());
+});
