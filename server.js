1723ef5c589afddd061bea28433e845213ac6ad4
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2026-01-05 13:59:35 +0900

    Update server.js

--- server.js
+++ server.js
@@ -4,10 +4,15 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(__dirname));
+// ★ここが重要：web-bunny フォルダを配信対象にする
+const webDir = path.join(__dirname, "web-bunny");
 
+// 静的配信
+app.use(express.static(webDir));
+
+// / は必ず index.html を返す（GETできません対策）
 app.get("/", (_req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(webDir, "index.html"));
 });
 
 app.listen(PORT, () => {
