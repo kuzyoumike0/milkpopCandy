8d5880a77fc88f79f7fc1cda9e16fbf1ce9629fd
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2026-01-05 13:59:16 +0900

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,11 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// このリポジトリ直下を静的配信
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(__dirname));
+
+app.get("/", (_req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
