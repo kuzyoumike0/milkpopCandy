4db78dd8afcb92ca0a33a9d866f5dad256430ebc
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2026-01-06 12:40:28 +0900

    Update slot.js

--- web-bunny/slot.js
+++ web-bunny/slot.js
@@ -136,7 +136,7 @@
     const s = document.createElement("style");
     s.textContent = `
 #${PANEL_ID}{
-  --winTop: 34%;
+  --winTop: 38%;
   --winH: 34%;
   --uiBottom: 8.5%;
   --resBottom: 18.5%;
